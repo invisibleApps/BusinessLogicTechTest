e5a4ebe9a58b3a4bae8e24d3bed58174c565e847
invisible <adswork@mail.ru>
2018-07-24 13:29:21 +0300

    Changes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -10,16 +10,16 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the factorial`s value.");
+        System.out.println("Enter the factorial`s value. Valid range of value is beetween 0 and " + Integer.MAX_VALUE);
         String stringValue = scanner.next();
 
         boolean isDigit = StringUtils.isNumeric(stringValue);
-        if (isDigit) {
+        if (isDigit && Long.parseLong(stringValue) <= Integer.MAX_VALUE) {
             int value = Integer.parseInt(stringValue);
             System.out.print(new MultiThreadFactorial().calculate(value));
         } else
-            System.err.println("Wrong input. The input value must be is greater than or equal to 0 and must be integer.");
-
+            System.err.println("Wrong input. The input value must be is greater than or equal to 0 and must be integer. " +
+                    "Max value = " + Integer.MAX_VALUE);
 
     }
 
